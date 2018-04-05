15de58aeb89a72b2325ac9884295e491872cc975
Ajinkya Chalke <achalke@mail.sfsu.edu>
2018-04-05 15:39:39 -0700

    added common.cpp and common.h from master

--- common.cpp
+++ common.cpp
@@ -93,19 +93,19 @@ void apply_force( particle_t &particle, particle_t &neighbor , double *dmin, dou
     double r2 = dx * dx + dy * dy;
     if( r2 > cutoff*cutoff )
         return;
-	if (r2 != 0)
+    if (r2 != 0)
         {
-	   if (r2/(cutoff*cutoff) < *dmin * (*dmin))
-	      *dmin = sqrt(r2)/cutoff;
+       if (r2/(cutoff*cutoff) < *dmin * (*dmin))
+          *dmin = sqrt(r2)/cutoff;
            (*davg) += sqrt(r2)/cutoff;
            (*navg) ++;
         }
-		
+        
     r2 = fmax( r2, min_r*min_r );
     double r = sqrt( r2 );
  
     
-	
+    
     //
     //  very simple short-range repulsive force
     //
--- common.h
+++ common.h
@@ -15,6 +15,7 @@ const int SAVEFREQ = 10;
 //
 typedef struct 
 {
+  double idx_bin;	
   double x;
   double y;
   double vx;
